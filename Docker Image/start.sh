4a5c16205a1b4f3e10eb5e4888a03b61d99949e4
castorbarge <c.verhoog@barge-master.com>
2025-10-15 13:36:59 +0200

    Update start.sh

--- Docker Image/start.sh
+++ Docker Image/start.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+ORG=$ORG
 REPO=$REPO
-REG_TOKEN=$REG_TOKEN
+PAT_TOKEN=$PAT_TOKEN
 NAME=$NAME
+
+REG_TOKEN=$(curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" -H \
+  -H "Authorization: Bearer ${PAT_TOKEN} \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  "https://api.github.com/orgs/${ORG}/actions/runners/registration-token"
 
 cd /home/docker/actions-runner || exit
 ./config.sh --url https://github.com/${REPO} --token ${REG_TOKEN} --name ${NAME}
