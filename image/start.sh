47679506452f332a128a236bcf1219d3273445a9
castorbarge <c.verhoog@barge-master.com>
2025-10-15 14:16:56 +0200

    use jq to access token

--- image/start.sh
+++ image/start.sh
@@ -6,12 +6,14 @@ REPO=$REPO
 PAT_TOKEN=$PAT_TOKEN
 NAME=$NAME
 
-REG_TOKEN=$(curl -L \
+result=$(curl -L \
   -X POST \
   -H "Accept: application/vnd.github+json" \
   -H "Authorization: Bearer ${PAT_TOKEN}" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
   "https://api.github.com/orgs/${ORG}/actions/runners/registration-token")
+
+REG_TOKEN=$(echo result | jq '.token')
 echo "Retrieved reg_token: ${REG_TOKEN}"
 
 echo "Running config"
